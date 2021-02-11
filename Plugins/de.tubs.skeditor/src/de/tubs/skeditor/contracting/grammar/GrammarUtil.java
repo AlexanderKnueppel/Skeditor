d4ca3df358b12a45b681de91c1f454988ed77947
Alexander Knueppel <a.knueppel@tu-bs.de>
2021-02-11 17:58:57 +0100

    Test stuff

--- Plugins/de.tubs.skeditor/src/de/tubs/skeditor/contracting/grammar/GrammarUtil.java
+++ Plugins/de.tubs.skeditor/src/de/tubs/skeditor/contracting/grammar/GrammarUtil.java
@@ -1,6 +1,10 @@
 package de.tubs.skeditor.contracting.grammar;
 
+import com.microsoft.z3.*;
+
+import java.util.ArrayList;
 import java.util.List;
+
 import java.util.Set;
 
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -52,5 +56,62 @@ public class GrammarUtil {
 			System.out.println(e.getMessage());
 		}
 		
+		
+//        System.out.println("Simplify Formula");
+//        //Log.append("Simplify Formula");
+//
+//        {
+//            Context ctx = new Context();
+//            //Tactic css = ctx.mkTactic("ctx-solver-simplify");
+//            
+//            Solver s = ctx.mkSolver();
+//            
+//            //x == 5 && x == 6 && x == 7 && x == 8 && x > 5
+//            
+//            IntExpr x = ctx.mkIntConst("x");
+//            
+//            BoolExpr c1 = ctx.mkEq(x, ctx.mkInt(5));
+//            BoolExpr c2 = ctx.mkEq(x, ctx.mkInt(6));
+//            BoolExpr c3 = ctx.mkEq(x, ctx.mkInt(7));
+//            BoolExpr c4 = ctx.mkEq(x, ctx.mkInt(8));
+//            BoolExpr c5 = ctx.mkEq(x, ctx.mkInt(9));
+//            
+//            BoolExpr c6 = ctx.mkOr(c1,c2,c3,c4,c5);
+//            BoolExpr c7 = ctx.mkAnd(c6, ctx.mkGt(x, ctx.mkInt(6)));
+//            
+//            Goal goal = ctx.mkGoal(true, false, false);
+//            goal.add(c7);
+//            
+//            Tactic simplify = ctx.mkTactic("simplify");
+//            Tactic ctxSimplify = ctx.mkTactic("ctx-simplify");
+//            Tactic ctxSolverSimplify = ctx.mkTactic("ctx-solver-simplify");
+//         
+//            ApplyResult res1 = simplify.apply(goal);
+//            ApplyResult res2 = ctxSimplify.apply(goal);
+//            ApplyResult res3 = ctxSolverSimplify.apply(goal);
+//            
+//            
+//            System.out.println("original term:\n" + c7 + "\n");
+//            System.out.println("simplify:\n" + res1 + "\n");
+//            System.out.println("ctxSimplify:\n" + res2 + "\n");
+//            System.out.println("ctxSolverSimplify:\n" + res3 + "\n");
+//            
+//            for(Goal sub : res3.getSubgoals()) {
+//            	s.add(sub.getFormulas());
+//            }
+//            
+//        	List<String> conditions = new ArrayList<String>();
+//        	for(BoolExpr expr : s.getAssertions()) {
+//        		String cond = "";
+//        		//expr.isG
+//        	}
+//            
+//            if (s.check() == Status.SATISFIABLE)
+//            	System.out.println("Model: " + s.getModel().toString());
+//            else
+//            	System.out.println("Not satisfiable!");
+//            
+//            ctx.close();
+//        }
 	}
 }
